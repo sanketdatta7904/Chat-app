2d9bcfd299375bab2e8d318707c0e8a23011c8c4
sanketdatta7904 <sanketdatta7904@gmail.com>
2022-08-16 20:44:27 +0530

    Added frontend and backedn inside one directory

---
+++ api/src/app.ts
@@ -0,0 +1,141 @@
+import express from 'express';
+import path from 'path';
+import { ApiRouting } from './api.routing.js';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import _ from "lodash"
+import {Config} from './config/config.js'
+// import pusher from "pusher"
+import Pusher from 'pusher';
+
+
+// const requestIp = require('request-ip');
+class App {
+  public express: express.Express;
+  // public routePrv: Routes = new Routes();
+
+  constructor() {
+    this.database();
+    this.express = express();
+    this.middleware();
+    this.initializeControllers();
+
+  }
+
+  /**
+   * database connection
+   */
+  private async database() {
+    const connectionUrl = Config.mongo.connectionUrl
+    // mongoose.connect(connectionUrl, {
+    //     autoIndex: true,
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true
+    // })
+
+    async function run() {
+      // 4. Connect to MongoDB
+      mongoose.connect(connectionUrl, () => {
+        console.log("connected to database")
+      })
+      const db = mongoose.connection
+      const pusher = new Pusher(Config.pusher);
+      db.once("open", () => {
+        console.log("DB Watch started");
+        const roomCollection = db.collection("rooms")
+        const roomStream = roomCollection.watch([], { fullDocument: 'updateLookup' })
+        roomStream.on("change", (change: any) => {
+          const roomDetails = change.fullDocument
+          let res: any = {}
+          let mongoEvent = ""
+          if (change.operationType === "insert") {
+
+            res = {}
+            mongoEvent = "create"
+            pusher.trigger("rooms", "inserted", {
+              name: roomDetails.name,
+              _id: roomDetails._id,
+              mongoEvent: mongoEvent,
+              messages: res
+            })
+
+          } else if (change.operationType === "update") {
+
+            if (_.get(change, ["updateDescription", "updatedFields", "messages"])) {
+              let newMessage = _.get(change, ["updateDescription", "updatedFields", "messages"])
+              res = newMessage[0]
+
+            } else {
+              res = _.get(change, ["updateDescription", "updatedFields"], {})
+
+              res = Object.values(res)[0]
+            }
+            mongoEvent = "update"
+            pusher.trigger("rooms", "updated", {
+              name: roomDetails.name,
+              _id: roomDetails._id,
+              mongoEvent: mongoEvent,
+              messages: res
+            })
+          }else if (change.operationType === "delete") {
+
+            res = {}
+            mongoEvent = "delete"
+            pusher.trigger("rooms", "deleted", {
+              _id: change.documentKey._id,
+            })
+
+          }
+          
+        })
+      })
+
+
+    }
+    run().catch(err => console.log(err));
+  }
+
+  /**
+   * http(s) request middleware
+   */
+  private middleware(): void {
+    const corsOption = {
+      credentials: true,
+      exposedHeaders: ['x-auth-token'],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      origin: true
+    };
+    this.express.use(cors(corsOption));
+    this.express.use(express.urlencoded());
+    this.express.use(express.json());
+    // this.express.use(requestIp.mw())
+    this.express.use((err, req, res, next) => {
+      console.log(err.toString());
+      next();
+    });
+  }
+
+  /**
+   * app environment configuration
+   */
+  // private setEnvironment(): void {
+  //   // dotenv.config({ path: ".env" });
+  //   global['__viewDir__'] = __dirname + '/views';
+  // }
+
+  /**
+   * API main routes
+   */
+  private initializeControllers() {
+    this.InitRoute();
+    this.express.use('*', (req, res) => {
+      res.status(404).send({ error: `\/${req.baseUrl}\/ doesn't exist` });
+    });
+  }
+
+  private InitRoute() {
+    ApiRouting.Register(this.express);
+  }
+}
+
+export default new App().express;
---
+++ api/src/controllers/rooms.controller.ts
@@ -0,0 +1,78 @@
+import { Router } from 'express';
+import { RoomsService } from '../services/rooms.service.js';
+export class ChatAppController {
+  public static route = '/v1';
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+    this.init();
+  }
+
+  public init() {
+    this.router.get('/rooms/sync', this.getRooms);
+    this.router.get('/rooms/:roomId/lastMessage', this.getLastMessage);
+    this.router.get('/rooms/:roomId', this.getAllMessages);
+    this.router.post('/rooms/new', this.createRoom);
+    this.router.post('/rooms/:roomId/newMessage', this.createNewMessage);
+    this.router.delete('/rooms/:roomId', this.deleteRoom);
+
+
+  }
+
+  private getRooms = async (request, response, next) => {
+    try {
+      const res = await new RoomsService().getRooms();
+      response.status(200).send(res);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  }
+  private deleteRoom = async (request, response, next) => {
+    try {
+      const roomId = request.params.roomId
+      const res = await new RoomsService().deleteRoom(roomId);
+      response.status(200).send(res);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  }
+  private getLastMessage = async (request, response, next) => {
+    try {
+      const roomId = request.params.roomId
+      const res = await new RoomsService().getLastMessage(roomId)
+      response.status(200).send(res);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  }
+  private getAllMessages = async (request, response, next) => {
+    try {
+      const roomId = request.params.roomId
+      const res = await new RoomsService().getAllMessages(roomId);
+      response.status(200).send(res);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  }
+
+  private createRoom = async (request, response, next) => {
+    try {
+      const room = request.body
+      const res = await new RoomsService().createRoom(room);
+      response.status(200).send(res);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  }
+  private createNewMessage = async (request, response, next) => {
+    try {
+      const roomId = request.params.roomId
+      const newMessage = request.body
+      const res = await new RoomsService().createNewMessage(roomId, newMessage)
+      response.status(200).send(res);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  }
+}
